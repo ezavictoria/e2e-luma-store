379f719d4986c864698d1f7f2b8d7a1e94e4d505
Eza Victória Amaral <ezalobo@outlook.com>
2024-03-08 17:03:01 -0300

    Writing contact form automation scenarios

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,37 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import lumaStoreElements from "./elements/lumaStoreElements"
+const LumaStoreElements = new lumaStoreElements()
+
+const lastName = Cypress.env('lastName')
+const email = Cypress.env('email')
+const zipCode = Cypress.env('zipCode')
+
+Cypress.Commands.add('fillInTheForm', (dropdownOption, orderID) => {
+    cy.get(LumaStoreElements.orderIDField()).type(orderID, {log: false})
+    cy.get(LumaStoreElements.billingLastNameField()).type(lastName, {log: false})
+    cy.get(LumaStoreElements.findOrderByDropdown()).select(dropdownOption)
+    if (dropdownOption == 'Email'){
+        cy.get(LumaStoreElements.emailField()).type(email, {log: false})
+    } else if (dropdownOption == 'ZIP Code'){
+        cy.get(LumaStoreElements.zipCodeField()).type(zipCode, {log: false})
+    }
+})
+    
+Cypress.Commands.add('generateRandomId', (length) => {
+    let result = '';
+    const characters = '0123456789';
+  
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+  
+    return result;
+  });
+
+Cypress.Commands.add('checkRequiredFields', () => {
+    cy.get(LumaStoreElements.orderIDError()).should('have.text', 'This is a required field.')
+    cy.get(LumaStoreElements.billingLastNameError()).should('have.text', 'This is a required field.')
+    cy.get(LumaStoreElements.emailError()).should('have.text', 'This is a required field.')
+    cy.get(LumaStoreElements.findOrderByDropdown()).select('ZIP Code')
+    cy.get(LumaStoreElements.continueButton()).click()
+    cy.get(LumaStoreElements.zipCodeError()).should('have.text', 'This is a required field.')
+})
